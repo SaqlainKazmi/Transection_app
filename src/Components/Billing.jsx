8877eee9c153673844cbcefe4c636a7615948f41
Saqalin Kazmi <saqlainkazmi12120@gmail.com>
2023-11-10 12:48:23 -0800

    first commit of transection app

---
+++ src/Components/Billing.jsx
@@ -0,0 +1,19 @@
+import {apple, bill, google} from '../assets';
+import styles, {layout} from '../style';
+
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img
+        src={bill}
+        alt="billing"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+
+      <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] white__gradient'/>
+      <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] pink__gradient'/>
+    </div>
+  </section>
+);
+
+export default Billing
